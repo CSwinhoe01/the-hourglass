7ea7477ae2d64d116b2a65ac74559a7f25104d33
CSwinhoe01 <Swinhoe09@live.co.uk>
2025-10-02 22:09:02 +0100

    add Import views & HttpResponse

--- TheHourglass/TheHourglass/urls.py
+++ TheHourglass/TheHourglass/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo_list import views as todo_list_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', todo_list_views.hello_todo_list, name='hello_todo_list'),
 ]
--- TheHourglass/todo_list/views.py
+++ TheHourglass/todo_list/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+def hello_todo_list(request):
+    return HttpResponse("Hello, to-do list!")
